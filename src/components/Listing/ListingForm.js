969e2f896b1854ce5a48cd5dd46fcea04965d7d4
Josiah Roa <josiahroa18@gmail.com>
2020-03-04 10:25:28 -0700

    Re-formatted input forms and listing cards

--- src/components/Listing/ListingForm.js
+++ src/components/Listing/ListingForm.js
@@ -7,74 +7,63 @@ import ListingCard from './ListingCard';
 import {ResponsiveContainer, Form, Input, Label, Error} from '../PresentationalComponents';
 
 // Material UI Imports
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+// import Autocomplete from '@material-ui/lab/Autocomplete';
+// import { makeStyles } from '@material-ui/core/styles';
+// import TextField from '@material-ui/core/TextField';
 
 // Auto Complete Multiple Input Styles
-const useStyles = makeStyles(theme => ({
-    root: {
-      margin: '20px 0',
-      width: '50%',
-      maxWidth: '400px',
-      minWidth: '280px',
-      border: 0,
-      boxShadow: '0px 0px 5px 0px rgba(0,0,0,0.75)',
-      borderRadius: '5px',
-      '& > * + *': {
-        marginTop: theme.spacing(3),
-      },
-    }
-  }));
+// const useStyles = makeStyles(theme => ({
+//     root: {
+//       margin: '20px 0',
+//       width: '50%',
+//       maxWidth: '400px',
+//       minWidth: '280px',
+//       border: 0,
+//       boxShadow: '0px 0px 5px 0px rgba(0,0,0,0.75)',
+//       borderRadius: '5px',
+//       '& > * + *': {
+//         marginTop: theme.spacing(3),
+//       },
+//     }
+//   }));
 
 /*
     Listing Form
     @return: Returns a form that accepts input for adding or editing a listing
 */
 function ListingForm(props){
-    const classes = useStyles();
+    // const classes = useStyles();
 
     const {id} = useParams();
-    const [ autoComplete, setAutoComplete] = useState(props.listings.find(listing => listing.id === parseInt(id, 10).features));
+    // const [ autoComplete, setAutoComplete] = useState(props.listings.find(listing => listing.id === parseInt(id, 10).features));
 
     const { register, handleSubmit, errors, watch } = useForm({defaultValues: {
         ...props.listings.find((listing) => listing.id === parseInt(id, 10))
     }});
 
-
     const onSubmit = (data, e) => {
-        const listing = {...data, features: [...autoComplete]};
-        props.edit ? props.editListing(listing) : props.addListing(listing)
+        // const listing = {...data, features: [...autoComplete]};
+        props.edit ? props.editListing(data) : props.addListing(data)
         e.target.reset();
     }
     
     return(
         <ResponsiveContainer>
-        <ListingCard preview={true} listing={{location: watch('location', 'location'), type: watch('type', 'type'), nights: watch('nights', 'nights')}}/>
+        <ListingCard preview={true} listing={{title: watch('title', 'title'), type: watch('type', 'type'), nights: watch('nights', 'nights')}}/>
         <Form onSubmit={handleSubmit(onSubmit)}>
-            <Label>Location</Label>
+            <Label>Title</Label>
             <Input
-                name='location'
+                name='title'
                 ref={register({
                     required: true,
-                    minLength: 6
+                    minLength: 4
                 })}
             />
-            {errors.location && errors.location.type === 'required' && <Error margin>Please enter a location</Error>}
-            {errors.location && errors.location.type === 'minLength' && <Error margin>Location must be at least 4 characters long</Error>}
-            <Label>Room Type</Label>
+            {errors.title && errors.title.type === 'required' && <Error margin>Please enter a title</Error>}
+            {errors.title && errors.title.type === 'minLength' && <Error margin>Title must be at least 4 characters long</Error>}
+            <Label>Bedrooms</Label>
             <Input
-                name='type'
-                ref={register({
-                    required: true,
-                    minLength: 3
-                })}
-            />
-            {errors.type && errors.type.type === 'required' && <Error margin>Please enter a room type</Error>}
-            {errors.type && errors.type.type === 'minLength' && <Error margin>Room type must be at least 3 characters long</Error>}
-            <Label>Minimum Nights Required</Label>
-            <Input
-                name='nights'
+                name='bedrooms'
                 ref={register({
                     required: true,
                     pattern: {
@@ -82,9 +71,21 @@ function ListingForm(props){
                     }
                 })}
             />
-            {errors.nights && errors.nights.type === 'required' && <Error margin>Please enter a minimum amount of nights</Error>}
-            {errors.nights && errors.nights.type === 'pattern' && <Error margin>Minimum nights must be a number</Error>}
-            <Label>Features</Label>
+            {errors.bedrooms && errors.bedrooms.type === 'required' && <Error margin>Please enter number of bedrooms</Error>}
+            {errors.bedrooms && errors.bedrooms.type === 'pattern' && <Error margin>Number of bedrooms must be a number</Error>}
+            <Label>Bathrooms</Label>
+            <Input
+                name='bathrooms'
+                ref={register({
+                    required: true,
+                    pattern: {
+                        value: /^[0-9]*$/gm
+                    }
+                })}
+            />
+            {errors.bathrooms && errors.bathrooms.type === 'required' && <Error margin>Please enter number of bathrooms</Error>}
+            {errors.bathrooms && errors.bathrooms.type === 'pattern' && <Error margin>Number of bathrooms must be a number</Error>}
+            {/* <Label>Features</Label>
             <Autocomplete
                 className = {classes.root}
                 onChange={(event, value) => setAutoComplete(value)}
@@ -99,7 +100,7 @@ function ListingForm(props){
                     {...params}
                 />
                 )}
-            />
+            /> */}
 
             <Input type='submit' className='submit-button' value={`${props.edit ? 'Edit Listing' : 'Add Listing'}`}/>
         </Form>
